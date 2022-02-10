4fe9c063218720b4147f51d500358c5fd2ce0b8c
Rahul Gupta <rahul26021999@gmail.com>
2022-02-10 14:53:41 +0530

    background js changed and file name changed

--- background.js
+++ background.js
@@ -1,16 +1,67 @@
-let color = '#3aa757';
+let mediaRecorder, stream;
 
-chrome.runtime.onInstalled.addListener(() => {
-    chrome.storage.sync.set({ color });
-    console.log('Default background color set to %cgreen', `color: ${color}`);
-
-
+chrome.runtime.onMessage.addListener((message, sender, callback) => {
+    console.log(message);
+    if (message.data === "start") {
+        console.log("startcalled");
+        recordScreen();
+        setTimeout(() => {
+            callback({ response: "Started" });
+        }, 1000);
+    } else if (message.data === "stop") {
+        console.log("stopcalled");
+        mediaRecorder.stop();
+        stopRecord();
+    }
+    return true;
 });
 
-chrome.extension.onConnect.addListener(function(port) {
-    console.log("Connected .....");
-    port.onMessage.addListener(function(msg) {
-        console.log("message recieved" + msg);
-        port.postMessage("Hi Popup.js");
+async function recordScreen() {
+
+    const displayStream = await navigator.mediaDevices.getDisplayMedia({ video: true, audio: true });
+    // voiceStream for recording voice with screen recording
+    const voiceStream = await navigator.mediaDevices.getUserMedia({ audio: true, video: false });
+    let tracks = [...displayStream.getTracks(), ...voiceStream.getAudioTracks()]
+    stream = new MediaStream(tracks);
+    createRecorder();
+}
+
+
+function createRecorder() {
+    // the stream data is stored in this array
+    let recordedChunks = [];
+
+    mediaRecorder = new MediaRecorder(stream);
+
+    mediaRecorder.ondataavailable = function(e) {
+        if (e.data.size > 0) {
+            recordedChunks.push(e.data);
+        }
+    };
+    mediaRecorder.onstop = function() {
+        saveFile(recordedChunks);
+        recordedChunks = [];
+    };
+    mediaRecorder.start(200); // For every 200ms the stream data will be stored in a separate chunk.
+}
+
+function stopRecord() {
+    stream.getTracks() // get all tracks from the MediaStream
+        .forEach(track => track.stop()); // stop each of them
+}
+
+function saveFile(recordedChunks) {
+
+    const blob = new Blob(recordedChunks, {
+        type: 'video/webm'
     });
-})
+    let filename = window.prompt('Enter file name'),
+        downloadLink = document.createElement('a');
+    downloadLink.href = URL.createObjectURL(blob);
+    downloadLink.download = `${filename}.webm`;
+
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+    URL.revokeObjectURL(blob); // clear from memory
+    document.body.removeChild(downloadLink);
+}
--- popup.js
+++ popup.js
@@ -1,7 +1,25 @@
-var port = chrome.extension.connect({
-    name: "Sample Communication"
-});
-port.postMessage("Hi BackGround");
-port.onMessage.addListener(function(msg) {
-    console.log("message recieved" + msg);
-});
+let start = document.getElementById('start');
+let stop = document.getElementById('stop');
+
+
+start.addEventListener('click', async function() {
+
+    chrome.runtime.sendMessage({ data: "start" }, function(response) {
+        console.log(response);
+    });
+
+
+    let node = document.createElement("p");
+    node.textContent = "Started recording";
+    document.body.appendChild(node);
+})
+
+stop.addEventListener('click', function() {
+    chrome.runtime.sendMessage({ data: "stop" }, function(response) {
+        console.log(response);
+    });
+
+    let node = document.createElement("p");
+    node.textContent = "Stopped recording";
+    document.body.appendChild(node);
+})
